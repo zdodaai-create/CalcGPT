0931d720f0d3ce96558060b242792d6f6735c215
Azanth S <sazanth75@gmail.com>
2026-05-22 12:47:45 +0530

    add open-next config and wrangler.jsonc

---
+++ open-next.config.ts
@@ -0,0 +1,24 @@
+import type { OpenNextConfig } from 'open-next/types/open-next.js';
+import cache from '@opennextjs/cloudflare/kvCache';
+
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: 'cloudflare-node',
+      converter: 'edge',
+      incrementalCache: async () => cache,
+      tagCache: 'dummy',
+      queue: 'dummy',
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: 'cloudflare-edge',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
+    },
+  },
+};
+
+export default config;
