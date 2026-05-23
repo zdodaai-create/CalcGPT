d276dfd2ac804ef6d86f6ee4cb32f057565bfff8
Azanth S <sazanth75@gmail.com>
2026-05-23 11:58:30 +0530

    feat: use Next.js App router API route for ads.txt to bypass middleware intercepts

---
+++ src/app/ads.txt/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+  return new Response("google.com, pub-9176484091341823, DIRECT, f08c47fec0942fa0", {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+}
